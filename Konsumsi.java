e33b6ed3dac53f6af669515b934e451de3d48fe0
EfrilSilvi <127361837+EfrilSilvi@users.noreply.github.com>
2023-03-09 09:52:04 +0700

    Add files via upload

---
+++ Konsumsi.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan1_2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Konsumsi<M, I> {
+    private M m;
+    private I i;
+    
+    public M getM(){
+        return m;
+    }
+    
+    public I getI(){
+        return i;
+    }
+    
+    public void setKonsumsi (M makanan, I minuman){
+        this.m = makanan;
+        this.i = minuman;
+    }
+}
+
